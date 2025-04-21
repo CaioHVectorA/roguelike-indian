9ba3c0a45bef0656c253d81f7944061a02406c8e
CaioHVectorA <caihebatista@gmail.com>
2025-04-21 00:55:27 -0300

    Add color constants and player configuration

---
+++ src/consts/player.ts
@@ -0,0 +1,12 @@
+const SCALE = 1;
+const WIDTH = 39 * SCALE;
+const HEIGHT = 49 * SCALE;
+const HEALTH_INITAL = 100;
+const BASE_SPEED = 440;
+
+export const PLAYER = {
+  WIDTH,
+  HEIGHT,
+  HEALTH_INITAL,
+  BASE_SPEED,
+};
